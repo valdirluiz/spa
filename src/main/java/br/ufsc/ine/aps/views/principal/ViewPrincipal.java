afdf7b1146d3c14f982701f5e85997f2894ebde8
Valdir Luiz <arnholdvaldir@gmail.com>
2016-06-12 22:33:45 -0300

    botao na tela principal abrindo o formulario de cadastro de protocolo

--- src/main/java/br/ufsc/ine/aps/views/principal/ViewPrincipal.java
+++ src/main/java/br/ufsc/ine/aps/views/principal/ViewPrincipal.java
@@ -1,8 +1,17 @@
 package br.ufsc.ine.aps.views.principal;
 
+import br.ufsc.ine.aps.views.protocolo.ViewProtocolo;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 /**
@@ -13,5 +22,19 @@ public class ViewPrincipal implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    @FXML
+    private void handleCadastroProtocoloButtonAction(ActionEvent event) {
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(ViewProtocolo.class.getResource("protocolo.fxml"));
+            stage.setTitle("Home");
+            stage.setScene(new Scene(root, 800, 500));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
