a5c61caa6148cf98af6edb668f785faf3d0fbc0a
Valdir Luiz <arnholdvaldir@gmail.com>
2016-06-21 01:42:49 -0300

    implementar cadastro de funcionarios

---
+++ src/main/java/br/ufsc/ine/aps/views/funcionario/ViewListFuncionario.java
@@ -0,0 +1,16 @@
+package br.ufsc.ine.aps.views.funcionario;
+
+import br.ufsc.ine.aps.views.pessoa.ViewPessoa;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class ViewListFuncionario extends ViewPessoa implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
