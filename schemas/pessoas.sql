28b487a63871e1792bee3def0bcc6aa0b66fdbac
Valdir Luiz <arnholdvaldir@gmail.com>
2016-06-25 14:49:04 -0300

    alterar schema de pessoa

--- schemas/pessoas.sql
+++ schemas/pessoas.sql
@@ -11,9 +11,8 @@
   data_cadastro date
  );
 
- CREATE UNIQUE INDEX pessoas_id_tipo_usuario_uindex ON pessoas(id,tipo_usuario);
- alter table add gerente INTEGER ;
+CREATE UNIQUE INDEX pessoas_id_tipo_usuario_uindex ON pessoas(id,tipo_usuario);
+alter table add gerente INTEGER ;
 
- insert into pessoas VALUES (11, '663.527.824-93', 'gerente@email.com', 'Gerente', '123', '5555', 4, 0, null, null);
-
+insert into pessoas (cpf, email, nome, senha, telefone, tipo_usuario, is_cliente) VALUES ('663.527.824-93', 'gerente@email.com', 'Gerente', '123', '5555', 4, 0);
 
