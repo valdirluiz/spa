0bdd7eb329f7d58259e2da6c51f8711d38d5e8f9
Rafael Ignácio <rafael.nacio@gmail.com>
2016-07-03 21:03:16 -0300

    Rafael Ignacio - Separacao do cadastro/atualizacao de clientes e contrutores para protocolo, operador e atendente

--- src/main/java/br/ufsc/ine/aps/models/Operador.java
+++ src/main/java/br/ufsc/ine/aps/models/Operador.java
@@ -1,5 +1,20 @@
 package br.ufsc.ine.aps.models;
 
+import br.ufsc.ine.aps.enuns.TipoUsuario;
+
 public class Operador extends Pessoa {
+
+    public Operador() {
+        super.setTipoUsuario(TipoUsuario.OPERADOR_SUPORTE);
+    }
+
+    public Operador(Integer id, String nome, String cpf, String email, String telefone) {
+        super.setId(id);
+        super.setNome(nome);
+        super.setCpf(cpf);
+        super.setEmail(email);
+        super.setTelefone(telefone);
+        super.setTipoUsuario(TipoUsuario.OPERADOR_SUPORTE);
+    }
 
 }
